f91a84e8342c731c47ebba6ead40aec1efecba9b
Tomas vc <jtomasvergarac@gmail.com>
2018-11-03 14:24:56 -0500

    operadores

---
+++ AprendiendoPHP/05operadores/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+// Operadores Aritmeticos
+
+$numero1 = 55;
+$numero2 = 33;
+
+
+echo "suma: ".($numero1+$numero2)."<br/>";
+echo "resta: ".($numero1-$numero2)."<br/>";
+echo "Multiplicacion: ".($numero1*$numero2)."<br/>";
+echo "Division: ".($numero1/$numero2)."<br/>";
