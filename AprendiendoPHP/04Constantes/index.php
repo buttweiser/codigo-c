5effeda1f7c9ebcb319b2d0be97c54fde398b8eb
Tomas vc <jtomasvergarac@gmail.com>
2018-11-03 14:24:48 -0500

    constantes

---
+++ AprendiendoPHP/04Constantes/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+//Contantes
+//es un contenedor de informacion cuyo valor no puede variar
+
+
+define('nombre','Tomas Vergara');
+define('web','Tomasvergara.com');
+
+echo nombre;
+echo web;
+
+//constantes predefinidas
+
+echo PHP_OS;
+echo PHP_VERSION;
