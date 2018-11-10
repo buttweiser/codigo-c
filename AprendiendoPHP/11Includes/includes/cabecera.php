b969c227c27362776b2e4244471a959a84612587
Tomas vc <jtomasvergarac@gmail.com>
2018-11-10 16:09:26 -0500

    includes en php

---
+++ AprendiendoPHP/11Includes/includes/cabecera.php
@@ -0,0 +1,8 @@
+<div class="cabecera">
+        <h1>includes en PHP</h1>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="yo.php">Yo</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
+        </ul>
+ </div>
---
+++ AprendiendoPHP/11Includes/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<!--cabecera-->
+
+    <?php include 'includes/cabecera.php';?>
+
+    <div class="contenido">
+        <h2>Esta es la pagina de inicio</h2>
+        <p>Texto de prueba de la pagina de inicio</p>
+    </div>
+
+    <?php include 'includes/footer.php'; ?>
+</body>
+</html>
